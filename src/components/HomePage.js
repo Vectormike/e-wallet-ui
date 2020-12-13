b138574c52d3460271cce3062d370ba0ef55b392
Victor Jonah <victorjonah199@gmail.com>
2020-12-14 00:37:14 +0100

    ...

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,13 @@
 import Navbar from './Navbar'
 import Feature from './Feature'
+import Footer from './Footer'
 
 function HomePage() {
   return (
     <div className='HomePage'>
       <Navbar />
       <Feature />
+      <Footer />
     </div>
   )
 }
