b169e9c1260891b94b3283860d41e1e16709be6e
Victor Jonah <victorjonah199@gmail.com>
2020-12-14 00:37:00 +0100

    ...

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,16 +1,42 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import Navbar from '../Navbar'
 import SideBar from '../Dashboard/SideBar'
 import View from '../Dashboard/View'
+import TopBar from '../Dashboard/TopBar'
+import Deposit from '../Deposit'
+
+const pages = [
+  {
+    component: Deposit,
+    path: '/deposit',
+    name: 'Deposit',
+  },
+  {
+    component: Deposit,
+    path: '/transfer',
+    name: 'Transfer',
+  },
+]
 
 const Dashboard = () => {
   return (
-    <div>
-      <Navbar />
-      <SideBar />
-      <View />
-    </div>
+    <Route
+      render={({ location }) => {
+        return (
+          <div>
+            <TopBar />
+            <SideBar location={location} pages={pages} />
+            <View>
+              {pages.map((page, i) => (
+                <Route key={i} exact path={page.path} component={page.component} />
+              ))}
+            </View>
+          </div>
+        )
+      }}
+    />
   )
 }
 
